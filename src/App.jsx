32cb4507994d380c91cafd65941633fcd252ca17
Anil Ozsoy <anilozsoy@gmail.com>
2024-11-07 14:07:37 +0100

    ✨ (App.jsx): add a title and description to the app 📝 (App.jsx): update the button styling and add a Google sign-in button with an icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import Panel from "./ui/panel";
+import GoogleIcon from "./components/icons/GoogleIcon";
 
 export default function App() {
   return (
     <div className="w-full h-full font-sans antialiased p-4">
-      <button
-        onClick={() => Panel()}
-        className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors mb-4"
-      >
-        Open Interface
-      </button>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <h1 className="text-2xl font-bold mb-2">HitMagnet</h1>
+        <p className="text-sm text-gray-500 mb-4 text-center">
+          Save YouTube video thumbnails to HitMagnet collections
+        </p>
+        <button
+          onClick={() => {
+            console.log("Google sign in clicked");
+          }}
+          className="w-full px-4 py-2 font-bold bg-white shadow-md text-gray-700 rounded-xl hover:shadow-xl transition-shadow mb-4 border border-zinc-100 flex items-center justify-center gap-2 hover:text-blue-500"
+        >
+          <GoogleIcon />
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 }
